559445a09ab6290549c7ceb4babdbb1386a6d5be
Tim Tawan <tim-tawan@hotmail.com>
2016-09-29 21:48:31 +1000

    first commit

---
+++ server/config.js
@@ -0,0 +1,7 @@
+const config = {
+  // mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
+  mongoURL: 'mongodb://admin:admin@ds035428.mlab.com:35428/docila',
+  port: process.env.PORT || 8000,
+};
+
+export default config;
